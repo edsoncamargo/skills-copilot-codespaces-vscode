f0ce5f8f38de5d1338e7876039cb674e084b0d41
Edson Camargo Menezes <42123898+edsoncamargo@users.noreply.github.com>
2025-02-26 22:10:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(name, age) {
+    const skills = {
+      name: name,
+      age: age,
+      languages: ['JavaScript', 'Python', 'Java'],
+      calculateNumbers: function (var1, var2) {
+        return calculateNumbers(var1, var2);
+      },
+    };
+   
+    return skills;
+}
